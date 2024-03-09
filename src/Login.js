9f2c0ba4cbd32dc70cbfebabffb6534574308193
N.Rahul Reddy <116450596+reddyy76@users.noreply.github.com>
2024-03-09 22:49:59 +0530

    Update Login.js

--- src/Login.js
+++ src/Login.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
+//Login page
+
 const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
